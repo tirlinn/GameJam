201ee94d11602d56f9b628ffe74fb78d1ab445b2
Tirlinn <kerantor37@gmail.com>
2021-01-29 16:14:13 +0600

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        body.velocity = new Vector2(inputX * speed, inputY * speed);
+    }
+}
